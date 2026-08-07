8e1cfb974cb66db82bcbfbe6f1e7d587829a3f55
blerner5 <blerner5@student.touro.edu>
2026-08-07 15:03:23 -0400

    Restored DontTapRed resources and added action_settings

--- app/src/main/java/com/mintedtech/dont_tap_red/activities/MainActivity.java
+++ app/src/main/java/com/mintedtech/dont_tap_red/activities/MainActivity.java
@@ -141,6 +141,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            // Settings logic if any
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 }
